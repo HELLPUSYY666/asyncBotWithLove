5db134d5b68c70bc7328e007c38f2c13bef3ba1e
Zakariya <pzakariya@mail.ru>
2025-01-21 23:24:34 +0500

    First commit for masya

--- bot.py
+++ bot.py
@@ -6,10 +6,3 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from config_reader import get_config, BotConfig, LogConfig
-from logs import get_structlog_config
-from structlog.typing import FilteringBoundLogger
-
-from dispatcher import dp
-import handlers
-
