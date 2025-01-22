1b2ee112b6039b86e6b0819495e7bfaee1b28e32
Zakariya <pzakariya@mail.ru>
2025-01-22 11:21:12 +0500

    Меняю структуру прокта под tgbot3

--- MasyaMAIN/bot.py
+++ MasyaMAIN/bot.py
@@ -6,3 +6,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
+
+async def main():
+    log_config: LogConif = get_config(model=LogConfig, root_key='logs')
---
+++ MasyaMAIN/logs.py
@@ -0,0 +1,7 @@
+import logging
+from json import dumps
+
+import structlog
+from structlog import WriteLoggerFactory
+
+from config_reader import L
