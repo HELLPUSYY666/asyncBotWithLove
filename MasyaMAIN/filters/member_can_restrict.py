7eeb560d0d3ba9d60399c33caba7e7d8ca2f308f
Zakariya <pzakariya@mail.ru>
2025-01-27 20:21:34 +0500

    Добавил филтр member_can_restrict.py

---
+++ MasyaMAIN/filters/member_can_restrict.py
@@ -0,0 +1,12 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+
+
+class MemberCanRestrictFilter(BaseFilter):
+    def __init__(self, member_can_restrict: bool):
+        self.member_can_restrict = member_can_restrict
+
+    async def __call__(self, message: Message):
+        member = await message.bot.get_chat_member(message.chat.id, message.from_user.id)
+
+        return (member.is_chat_creator() or member.can_restrict_members) == self.member_can_restrict
