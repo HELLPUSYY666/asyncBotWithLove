e03516d44fe382518763fa55f8a9218504493563
Zakariya <pzakariya@mail.ru>
2025-01-27 19:42:08 +0500

    Теперь уже в испольняем файле добавил логи

--- MasyaMAIN/bot.py
+++ MasyaMAIN/bot.py
@@ -5,11 +5,14 @@ import structlog
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from config_reader import LogConfig, get_config
+from config_reader import *
 from logs import get_structlog_config
 
 
 async def main():
     log_config: LogConfig = get_config(model=LogConfig, root_key='logs')
     structlog.configure(**get_structlog_config(log_config))
+
+    bot_config: BotConfig = get_config(model=BotConfig, root_key="bot")
+
 
