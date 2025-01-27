9bb74665f31d610af02977a90dcab3d7b1abe4da
Zakariya <pzakariya@mail.ru>
2025-01-27 20:19:58 +0500

    Добавил __init__ для хэндлеров

---
+++ MasyaMAIN/handlers/__init__.py
@@ -0,0 +1,7 @@
+from MasyaMAIN.dispatcher import dp
+
+from . import admin_actions, group_events, personal_actions
+
+dp.include_router(admin_actions.router)
+dp.include_router(group_events.router)
+dp.include_router(personal_actions.router)
