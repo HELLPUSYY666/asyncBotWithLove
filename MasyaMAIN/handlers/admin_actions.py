ba0eabc71bdb1ba05459562e7f97d1891af49763
Zakariya <pzakariya@mail.ru>
2025-01-27 20:20:13 +0500

    Добавил admin_actions

---
+++ MasyaMAIN/handlers/admin_actions.py
@@ -0,0 +1,29 @@
+import structlog
+from aiogram import Router, F, Bot
+from aiogram.filters import Command, CommandStart, CommandObject
+from aiogram.types import Message, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from fluent.runtime import FluentLocalization
+from MasyaMAIN.filters.is_owner import IsOwnerFilter
+
+router = Router()
+router.message.filter(F.chat.type == "private",
+                      IsOwnerFilter(is_owner=True))
+
+logger = structlog.get_logger()
+
+
+@router.message(Command("start"))
+async def cmd_owner_hello(message: Message, l10n: FluentLocalization):
+    await message.answer(
+        l10n.format_value("hello-owner")
+    )
+
+
+@router.message(
+    IsOwnerFilter(is_owner=True),
+    Command(commands=["ping"]),
+)
+async def cmd_ping_bot(message: Message, l10n: FluentLocalization):
+    await message.reply(l10n.format_value("ping-msg"))
